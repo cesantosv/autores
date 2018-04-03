9e93e51844d651038f86767a312798424c285f3a
cesantosv <cesantosv@unal.edu.co>
2018-04-02 22:21:11 -0500

    componente funcionando

--- app/serializers/author.js
+++ app/serializers/author.js
@@ -1,11 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
-    
+
     keyForAttribute(attr) {
         switch (attr) {
             case 'name':
-            return 'name';
+                return 'name';
             case 'id':
                 return 'id';
             case 'dateOfBirth':
@@ -21,5 +21,5 @@ export default DS.JSONAPISerializer.extend({
                 return this._super(...arguments);
         }
     },
-   
+
 });
